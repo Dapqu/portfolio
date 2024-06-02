f04e8a088911f684fd6e14a8bcf7cf656e5cde69
Dilimulati Diliyaer <dilyarswe@gmail.com>
2024-06-02 08:38:03 -0400

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
