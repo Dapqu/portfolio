55126f0f85e5a069fd981b3173ae085e30a0c70c
Dilimulati Diliyaer <dilyarswe@gmail.com>
2024-06-06 18:45:21 -0400

    Update page.jsx

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -12,7 +12,6 @@ import ProjectSliderBtns from "@/components/ProjectSliderBtns";
 
 const projects = [
     {
-        num: "01",
         title: "Void - GWJ #69",
         description: "Solo game development project created for Godot Wild Jam, completed in 9 days. In a race against the encroaching darkness, harness your void vision to navigate through the universe.",
         stack: [{ name: "Asperite" }, { name: "GD Script (Python)" }, { name: "Godot Engine" }],
@@ -21,7 +20,6 @@ const projects = [
         github: "https://github.com/Dapqu/Void-GWJ-69",
     },
     {
-        num: "02",
         title: "Portfolio Website",
         description: "Concise and clear portfolio website showcasing my education, professional experience, and skills. The current site itself is the live demonstration of the project.",
         stack: [{ name: "Css 3" }, { name: "Html 5" }, { name: "Javascript" }, { name: "Next.js" }, { name: "Node.js" }, { name: "React" }, { name: "Tailwind Css" }],
@@ -30,7 +28,6 @@ const projects = [
         github: "https://github.com/Dapqu/portfolio",
     },
     {
-        num: "03",
         title: "FlapAI",
         description: "Elevating the classic Flappy Bird concept by introducing new gameplay twists such as a hardmode. Leveraging the Unity game engine and the implementation of Artificial Intelligence algorithms such as PPO and Q-learning. This integration enabled the AI to autonomously navigate and play in the reimagined Flappy (Bird) AI.",
         stack: [{ name: "Asperite" }, { name: "C#" }, { name: "ML" }, { name: "Python" }, { name: "ShaderLab" }, { name: "Unity Engine" }],
@@ -39,7 +36,6 @@ const projects = [
         github: "https://github.com/Dapqu/FlapAI",
     },
     {
-        num: "04",
         title: "Programming Language",
         description: "Developed a programming language in Java comprising a lexer, parser, interpreter, analyzer, and generator. Produced effective test scripts leveraging JUnit to automate up to 100 tests. Incorporated streamlined functionalities including variables, functions, if/while/switch statements, and operators.",
         stack: [{ name: "Java" }, { name: "JUnit" }],
@@ -48,7 +44,14 @@ const projects = [
         github: "https://github.com/Dapqu/Programming-Language",
     },
     {
-        num: "05",
+        title: "Tennis Match Footage Analyzer (WIP)",
+        description: "Automated AI system for tracking players, tennis ball, and the court through any a  tennis match footage. By leveraging deep learning models and computer vision techniques, the system can identify players, track their movements, and overlay key court markers on the video frames. The project utilizes the YOLO object detection model for player tracking and a custom ResNet-based model for court line detection.",
+        stack: [{ name: "YOLO" } ,{ name: "Python" }, { name: "Pytorch" }, { name: "Jupyter Notebook" }, { name: "ML/AI" }, { name: "OpenCV" }, { name: "Pickle" }, { name: "Pandas" }],
+        image: "/assets/projects/12.png",
+        live: "N/A",
+        github: "https://github.com/Dapqu/tennis_match_footage_analyzer",
+    },
+    {
         title: "Interective 3D World Map (WIP)",
         description: "Developed an interactive 3D website using Three.js, showcasing a world map where users can hover over and select countries. Implemented features to dynamically change the background to the selected country's flag and provide relevant information upon selection.",
         stack: [{ name: "Blender" } ,{ name: "Css 3" }, { name: "Html 5" }, { name: "Javascript" }, { name: "Node.js" }, { name: "Three.js" }],
@@ -57,7 +60,6 @@ const projects = [
         github: "https://github.com/Dapqu/InterectiveWorldMap",
     },
     {
-        num: "06",
         title: "Memory Manager",
         description: "Implement a memory manager in C++, whose features include initializing, tracking, allocating, and deallocating sections of memory. Integrate the memory manager into the console-based testing program provided.",
         stack: [{ name: "C++" }],
@@ -66,7 +68,6 @@ const projects = [
         github: "https://github.com/Dapqu/Memory-Manager",
     },
     {
-        num: "07",
         title: "Covid Analyzer, Data Structure",
         description: "Worked with development team members to design and develop a Covid-19 analyzer to provide up to 100,000 testing/vaccination data. Designed and implemented two custom C++ data structures for the project to meet client requirements for performance comparisons. Achieved project deadlines by coordinating with contractors to manage performance.",
         stack: [{ name: "C++"}, { name: "Custom data structures"}],
@@ -75,7 +76,6 @@ const projects = [
         github: "https://github.com/Dapqu/covid-analyzer",
     },
     {
-        num: "08",
         title: "Cache Simulator",
         description: "The cache simulation reads addresses from trace files, converts hex to binary, and processes them based on the cache design. It separates the address into tag and offset, then inserts the tag into the cache. If the tag is present, it registers a hit; if not, and the cache is full, it uses FIFO or LRU replacement to free space before adding the new tag.",
         stack: [{ name: "C" }, { name: "C++" }],
@@ -84,7 +84,6 @@ const projects = [
         github: "https://github.com/Dapqu/Cache-Simulator",
     },
     {
-        num: "09",
         title: "P2P File Sharing Software",
         description: "This project is about writing a P2P file-sharing software similar to BitTorrent. Within this P2P protocol, it includes file distribution, choking-unchoking mechanism, and more.",
         stack: [{ name: "Java" }],
@@ -93,7 +92,6 @@ const projects = [
         github: "https://github.com/Dapqu/CNT4007",
     },
     {
-        num: "10",
         title: "Curb Your Emissions",
         description: "Developed, initiated, and managed the creation of a tycoon-like video game using C# in Unity. Tracked project and team member performance closely to quickly intervene in mistakes or delays. Utilized Agile software development methods to increase team efficiency by 20%.",
         stack: [{ name: "C#" }, { name: "ShaderLab"}, { name: "Unity Engine"}],
@@ -102,7 +100,6 @@ const projects = [
         github: "https://github.com/Dapqu/Curb-Your-Emissions",
     },
     {
-        num: "11",
         title: "Minesweeper",
         description: "Recreation of the classic minesweeper using C++.",
         stack: [{ name: "C++" }],
@@ -132,7 +129,7 @@ const Projects = () => {
                         <div className="flex flex-col gap-[30px] h-[50%]">
                             {/* Outline number */}
                             <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
-                                {project.num}
+                                {(projects.findIndex(p => p === project) + 1).toString().padStart(2, '0')}
                             </div>
                             {/* Project category */}
                             <h2 className="text-[42px] font-bold leading-none text-customColor group-hover:text-accent transition-all duration-500 capitalize">
